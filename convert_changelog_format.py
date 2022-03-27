a60c6c083e3b8f9eea72f8311a13dfc2ea2d2c1a
Eduardo Cristiano Thums <eduardocristiano01@gmail.com>
2022-03-27 16:11:53 -0300

    Create script to convert changelog xml

---
+++ convert_changelog_format.py
@@ -0,0 +1,43 @@
+import re
+import yaml
+
+
+def convert_from_xml_to_json():
+    migrations = []
+
+    with open('changelog.xml', 'r') as changelog:
+        for line in changelog.readlines():
+            migration = {}
+
+            match = re.findall(r'file="([^"]+)"', line)
+
+            if len(match) > 0:
+                migration['file'] = match[0]
+
+            match = re.findall(r'context="([^"]+)"', line)
+
+            if len(match) > 0:
+                context = match[0]
+
+                # remove all white spaces
+                # context = re.sub(r'\s+', '', context)
+
+                # split by comma
+                # context = context.split(',')
+
+                migration['context'] = context
+
+            if 'file' in migration:
+                migrations.append(migration)
+
+    return migrations
+
+
+def convert_from_xml_to_yaml():
+    migrations = convert_from_xml_to_json()
+
+    with open('changelog.yaml', 'w') as changelog:
+        yaml.dump(migrations, changelog, sort_keys=False)
+
+
+convert_from_xml_to_yaml()
