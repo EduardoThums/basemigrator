06de22d6bacb99420d80a914047428f65bd16f3e
Eduardo Cristiano Thums <eduardocristiano01@gmail.com>
2022-03-27 19:20:07 -0300

    Create setup.py file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from io import open
+
+requirements = []
+
+with open('requirements.txt') as f:
+    lines = f.read().splitlines()
+    for dependency in lines:
+        dependency = dependency.strip()
+
+        requirements.append(dependency)
+
+setup(
+    name='migrator',
+    version='0.0.1',
+    description='Migrator',
+    author='CWI Software',
+    url='https://github.com/EduardoThums/migrator',
+    python_requires=">=3.6, <4",
+    packages=find_packages(),
+    install_requires=requirements,
+    include_package_data=True
+)
